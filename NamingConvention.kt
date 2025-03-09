f802020ada32099706a49410704d76dd1773aef4
PravejXLab <pravejxlab@gmail.com>
2025-03-09 22:59:41 +0530

    Learned Naming Conventions to use!

---
+++ NamingConvention.kt
@@ -0,0 +1,24 @@
+fun main() {
+    // camelCase Naming Convention -- 
+
+    val userLanguage = "Kotlin"  // Used for variable names 
+    var accountBalance = 25000 
+    fun calculateTotalPrice() {}  // Used for functions naming  
+
+    println("UserLanguage: $userLanguage, AccountBalance: $accountBalance") 
+}
+
+
+
+// PascalCase Naming Convention -- 
+
+class LoveFromKotlin  // Used for class naming 
+object AndroidManager  // Used for object naming 
+interface OnClickListener  // Used for interface naming 
+
+
+
+// UPPER_SNAKE_CASE Naming Convention -- 
+
+const val API_KEY = "ABC123XYZ"  // Used for assigning constant values
+const val YOUR_PIN = 906578   // Just an example Pin 😅
